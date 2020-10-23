bba8dabf8e67c5ac7ffdeda0f2643bc673ec0e4c
Marc Stammerjohann <marcstammerjohann@web.de>
2020-10-23 16:35:34 +0200

    feat(scullly): add scully

---
+++ scully.angular-scully-tailwindcss.config.ts
@@ -0,0 +1,7 @@
+import { ScullyConfig } from '@scullyio/scully';
+export const config: ScullyConfig = {
+  projectRoot: './src',
+  projectName: 'angular-scully-tailwindcss',
+  outDir: './dist/static',
+  routes: {},
+};
