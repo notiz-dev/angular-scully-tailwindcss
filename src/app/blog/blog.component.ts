bfacc070d298c1fddd6b9a822b9048566d66a1fd
Marc Stammerjohann <marcstammerjohann@web.de>
2020-10-26 17:12:44 +0100

    feat(blog-post): display post header from front matter

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,19 +1,23 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {ActivatedRoute, Router, ROUTES} from '@angular/router';
-
-declare var ng: any;
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ScullyRoute, ScullyRoutesService } from '@scullyio/ng-lib';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.scss'],
   preserveWhitespaces: true,
-  encapsulation: ViewEncapsulation.Emulated
-
+  encapsulation: ViewEncapsulation.Emulated,
 })
 export class BlogComponent implements OnInit {
-  ngOnInit() {}
+  currentPost$: Observable<ScullyRoute> = this.scully.getCurrent();
 
-  constructor(private router: Router, private route: ActivatedRoute) {
-  }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private scully: ScullyRoutesService
+  ) {}
+
+  ngOnInit() {}
 }
