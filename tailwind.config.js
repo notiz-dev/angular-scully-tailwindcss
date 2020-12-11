594c829aeebb8dd852bcaf1c4ea6dafa615111fd
Marc Stammerjohann <marcstammerjohann@web.de>
2020-12-11 17:27:00 +0100

    purge scully markdown content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,31 @@
+const marked = require('marked');
+
 module.exports = {
-  purge: ['./src/**/*.{html,ts}'],
+  purge: {
+    mode: 'all',
+    preserveHtmlElements: false,
+    content: ['./src/**/*.{html,ts}', './blog/**/*.md'],
+    options: {
+      extractors: [
+        {
+          extensions: ['md'],
+          extractor: (content) => {
+            content = marked(content);
+
+            // Capture as liberally as possible, including things like `h-(screen-1.5)`
+            const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+
+            // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+            const innerMatches =
+              content.match(/[^<>"'`\s.(){}[\]#=%]*[^<>"'`\s.(){}[\]#=%:]/g) ||
+              [];
+
+            return broadMatches.concat(innerMatches);
+          },
+        },
+      ],
+    },
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
